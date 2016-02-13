79425a0b973b934c6b1a13061696e721b3a9b70d
Jan Weiß <jan@geheimwerk.de>
2016-02-14 00:39:22 +0100

    Adding changes due to CocoaPods update.

--- Pods/Headers/Private/RFOverlayScrollView/RFOverlayScrollView.h
+++
@@ -1,1 +0,0 @@
-../../../RFOverlayScrollView/RFOverlayScrollView/RFOverlayScrollView.h
---
+++ Pods/Target Support Files/Pods-SmartNoteUITests/Pods-SmartNoteUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SmartNoteUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SmartNoteUITests
+@end
